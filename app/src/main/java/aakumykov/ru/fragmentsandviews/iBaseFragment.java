71822a6ffb296abf811709562a4f1ab861799e98
Андрей Кумыков <aakumykov@yandex.ru>
2018-12-12 11:47:58 +0700

    Дополнительные методы у BaseFragment.

--- app/src/main/java/aakumykov/ru/fragmentsandviews/iBaseFragment.java
+++ app/src/main/java/aakumykov/ru/fragmentsandviews/iBaseFragment.java
@@ -3,7 +3,10 @@ package aakumykov.ru.fragmentsandviews;
 public interface iBaseFragment {
 
     void showProgressMessage(int messageId);
+
     void hideProgressMessage();
 
     void showToast(int messageId);
+
+    void showErrorMsg(int messageId);
 }
