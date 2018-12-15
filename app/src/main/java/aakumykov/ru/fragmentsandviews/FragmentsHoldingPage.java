64deebfcf9e8f076a7d43614a22a58d8222fdd3a
Андрей Кумыков <aakumykov@yandex.ru>
2018-12-15 11:02:33 +0700

    fix: проблема решена

--- app/src/main/java/aakumykov/ru/fragmentsandviews/FragmentsHoldingPage.java
+++ app/src/main/java/aakumykov/ru/fragmentsandviews/FragmentsHoldingPage.java
@@ -2,6 +2,8 @@ package aakumykov.ru.fragmentsandviews;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 
 import aakumykov.ru.fragmentsandviews.TEMPLATE.List_Fragment;
@@ -9,9 +11,10 @@ import aakumykov.ru.fragmentsandviews.boards_list.BoardsList_Fragment;
 import aakumykov.ru.fragmentsandviews.interfaces.iDvachPagesInteraction;
 import aakumykov.ru.fragmentsandviews.threads_list.ThreadsList_Fragment;
 
-public class FragmentsPage extends BaseView implements
+public class FragmentsHoldingPage extends BaseView implements
         iDvachPagesInteraction
 {
+    private FragmentManager fragmentManager;
     private boolean firstRun = true;
 
     @SuppressLint("CommitTransaction")
@@ -19,6 +22,7 @@ public class FragmentsPage extends BaseView implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.single_page_activity);
+        fragmentManager = getSupportFragmentManager();
     }
 
     @Override
@@ -34,9 +38,9 @@ public class FragmentsPage extends BaseView implements
     public void showBoardsOnDvach() {
         BoardsList_Fragment boardsListFragment = new BoardsList_Fragment();
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment_place, boardsListFragment);
-//        fragmentTransaction.addToBackStack(BoardsList_Fragment.TAG);
+        fragmentTransaction.addToBackStack(BoardsList_Fragment.TAG);
         fragmentTransaction.commit();
     }
 
@@ -45,12 +49,25 @@ public class FragmentsPage extends BaseView implements
         Bundle arguments = new Bundle();
         arguments.putString(Constants.BOARD_ID, boardId);
 
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
         ThreadsList_Fragment threadsListFragment = new ThreadsList_Fragment();
         threadsListFragment.setArguments(arguments);
 
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.addToBackStack(BoardsList_Fragment.TAG);
+        Fragment currentFragment = fragmentManager.findFragmentById(R.id.fragment_place);
+        if (null != currentFragment)
+            fragmentTransaction.hide(currentFragment);
+
         fragmentTransaction.add(R.id.fragment_place, threadsListFragment);
+        fragmentTransaction.addToBackStack(BoardsList_Fragment.TAG);
+
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_ENTER_MASK);
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_EXIT_MASK);
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+//        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+
         fragmentTransaction.commit();
     }
 
