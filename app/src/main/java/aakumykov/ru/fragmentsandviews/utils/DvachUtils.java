ef25c8f8d1e59197396cfe160ea0af82b6fd0c49
Андрей Кумыков <aakumykov@yandex.ru>
2018-12-19 21:48:41 +0700

    Методы коррекции html-символов комментария.

--- app/src/main/java/aakumykov/ru/fragmentsandviews/utils/DvachUtils.java
+++ app/src/main/java/aakumykov/ru/fragmentsandviews/utils/DvachUtils.java
@@ -10,13 +10,24 @@ public class DvachUtils {
     public static String preProcessComment(String text) {
         text = (text + "").trim();
         text = clearThreadComment(text);
+
+        text = specialchars2html(text); // должна идти пред br2nl (или запихать её внутрь?)
         text = br2nl(text);
+
         text = html2text(text);
         return text;
     }
 
     private static String clearThreadComment(String rawComment) {
         return rawComment.replaceFirst("<a[^>]*>.*</a>(<br>)*", "");
+    }
+
+    private static String specialchars2html(String inputString) {
+        inputString = inputString.replaceAll("&amp;", "&");
+        inputString = inputString.replaceAll("&lt;", "<");
+        inputString = inputString.replaceAll("&gt;", ">");
+        inputString = inputString.replaceAll("&nbsp;", " ");
+        return inputString;
     }
 
     private static String br2nl(String inputString) {
